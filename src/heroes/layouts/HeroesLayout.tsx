ad74ac2d41c1204e34d091a21fb547eb718dac26
Alberto Villarreal <Alberto.Villareal.Flores.-ND@disney.com>
2025-12-09 16:09:29 -0600

    first commit

---
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -0,0 +1,9 @@
+import { Link, Outlet } from "react-router"
+
+export const HeroesLayout = () => {
+    return (
+        <div>
+            <Outlet />
+        </div>
+    )
+}
---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,25 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+import { SearchControls } from './ui/SearchControls';
+//import { CustomBreadcrumbs } from '@/components/custom/CustomBreadcrumbs';
+
+export const SearchPage = () => {
+    return (
+        <>
+        <CustomJumbotron
+            title="Búsqueda de SuperHéroes"
+            description="Descubre, explora y administra super héroes y villanos"
+        />
+
+        
+
+        {/* Stats Dashboard */}
+        <HeroStats />
+
+        {/* Filter and search */}
+        <SearchControls />
+        </>
+    );
+};
+
+export default SearchPage;
